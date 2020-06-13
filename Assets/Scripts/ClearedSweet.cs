2b8164ac2ca09ca562d2232b190fd5c98fc72b6d
IzzyPrime <345601716@qq.com>
2020-06-13 17:20:48 +0800

    ReUpload

---
+++ Assets/Scripts/ClearedSweet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearedSweet : MonoBehaviour {
+
+    public AnimationClip clearAnimation;
+
+    private bool isClearing;
+
+    public AudioClip destoryAudio;
+
+    public bool IsClearing
+    {
+        get
+        {
+            return isClearing;
+        }
+    }
+
+    protected GameSweet sweet;
+
+    private void Awake()
+    {
+        sweet = GetComponent<GameSweet>();
+    }
+
+    public virtual void Clear()
+    {
+        isClearing = true;
+        StartCoroutine(ClearCoroutine());
+    }
+
+    private IEnumerator ClearCoroutine()
+    {
+        Animator animator = GetComponent<Animator>();
+
+        if (animator != null)//+1 清除音效
+        {
+            animator.Play(clearAnimation.name);
+            GameManager.Instance.playerScore++;
+            AudioSource.PlayClipAtPoint(destoryAudio, transform.position);
+            yield return new WaitForSeconds(clearAnimation.length);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# SweetsCrush
+## 消消乐游戏
+## Unity 2017.4.37c1
